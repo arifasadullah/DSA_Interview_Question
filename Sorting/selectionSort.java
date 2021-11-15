5c7bcf63c1393432bdd9db290f72adc8fae45e3a
arifasadullah123 <arifasadullah123@gmai.com>
2021-11-15 14:10:58 +0530

    first commit

--- Sorting/selectionSort.java
+++ Sorting/selectionSort.java
@@ -1,34 +1,31 @@
-public class selectionSort{
-	
-	//main function
-    public static void main(String [] args){
-	int [] arr={2,50,25,14,6,0,6,63};
-	selectionSort(arr);
-	printArr(arr);
-   }
-   
-   //code for selection Sort
-   public static void selectionSort(int[] arr){
-	int n=arr.length;
-	for(int i=0;i<n;i++){
-		int min_element_index=i;
-		for(int j=i+1;j<n;j++){
-			if(arr[min_element_index]>arr[j]){
-				//updating the min_element_index index
-				min_element_index=j;
-	}
-		}
-		//swap the element 
-	int temp=arr[min_element_index];
-	arr[min_element_index]=arr[i];
-	arr[i]=temp;
-	}
-}
+public class SelectionSort{
+	 public static void main(String[] args){
+        System.out.println("Array before sorting..");
+        int[] arr={2,6,1,5,0,5,10};
+        printArr(arr);
+        System.out.println();
+        selectionSort(arr);
+        System.out.println("Array after sorting..");
+        printArr(arr);
 
-//for printing the array
-public static void printArr(int [] arr){
-	for(int el:arr){
-		System.out.print(el+" ");
     }
-   }
+    public static void selectionSort(int[] arr){
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            int min_index=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[min_index]>arr[j]){
+                    min_index=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[min_index];
+            arr[min_index]=temp;
+        }
+    }
+    public static void printArr(int[] arr){
+        for(int el:arr){
+            System.out.print(el+" ");
+        }
+    }
 }
